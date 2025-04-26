53061cd1056ac1045e641658ee40653342d0f6e2
Rajkummar213 <rajkd832@gmail.com>
2025-04-26 04:53:44 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    //* jqury RUN
+
+    $('.bannerslider').slick({
+        prevArrow: "#bannerleftArrow",
+        nextArrow: "#bannerrightArrow",
+        dots: true,
+        dotsClass: "slider-dots",
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+    
+     
+  });
